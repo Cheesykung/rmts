77b76dc560fe0e88d8eed6ea08ff4565d48f4057
it62070122 <fookkub1@gmail.com>
2021-04-22 17:02:50 +0700

    to my computer

--- idgen.py
+++ idgen.py
@@ -1,5 +1,8 @@
 def main():
-    for i in range(192, 205+1):
+    # for i in range(90, 83, -1):
+    #     print("(%d, %d, %d),"%(i+1, i, i-1))
+
+    for i in range(105, 121+1):
         print("(%d, %d, %d),"%(i-1, i, i+1))
 
 main()
