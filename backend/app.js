891ec32974425d60755172159262c2f2127e4204
it62070122 <fookkub1@gmail.com>
2021-04-25 13:18:39 +0700

    add more environment
    -add config.js for connect to database
    -install package mySQL2

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
+// require package
 const express = require('express')
 
+// create instance
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// config format
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+//router
+const searchRouter = require('./api/searchAPI')
+
+// register router
+app.use(searchRouter.router)
+
+// open port
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
