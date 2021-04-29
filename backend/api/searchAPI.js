320d220db82c513297db7c66f65604f98abb3cb3
it62070122 <fookkub1@gmail.com>
2021-04-30 00:08:32 +0700

    send to my com

--- backend/api/searchAPI.js
+++ backend/api/searchAPI.js
@@ -6,14 +6,33 @@ const pool = require("../config");
 router = express.Router();
 
 router.get("/search", async (req, res, next) => {
-  const start = req.params.start;
-  const des = req.params.des;
-  const promise1 = pool.query("SELECT * FROM before_next");
+  const start_type = req.params.start_type
+  const begin = req.params.start
+  const des_type = req.params.des_type
+  const finish = req.params.des
+  let start = null;
+  let des = null;
 
-  Promise.all([promise1])
+  const promise = pool.query("SELECT station_id FROM stations WHERE station_name like '%?%' and type = ?", [begin, start_type])
+  promise.then((results) => {
+    const getData = results[0][0];
+    start = getData.station_id
+  })
+  console.log(start);
+
+  const promise2 = pool.query("SELECT station_id FROM stations WHERE station_name like '%?%' and type = ?", [finish, des_type])
+  promise2.then((results) => {
+    const getData = results[0][0];
+    des = getData.station_id
+  })
+  console.log(des);
+
+  const promise3 = pool.query("SELECT * FROM before_next");
+
+  Promise.all([promise3])
     .then((results) => {
-      const blogs = results[0][0];
-      let path = blogs.map((element) => {
+      const getData = results[0][0];
+      let path = getData.map((element) => {
         return [
           element.before3,
           element.before2,
@@ -49,11 +68,11 @@ router.get("/search", async (req, res, next) => {
       })
         .then((response) => {
           let payload = response;
-          res.status(200).send(payload.data);
           res.setHeader('Access-Control-Allow-Origin', '*');
           res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
           res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
           res.setHeader('Access-Control-Allow-Credentials', true);
+          res.status(200).send(payload.data);
         })
         .catch((err) => {
           res.status(400).send("Bad Request");
